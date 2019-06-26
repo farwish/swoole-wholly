8c3fd199a741304dcea4c1d8ef3f92da4c15db48
farwish <farwish@foxmail.com>
2019-06-26 23:26:32 +0800

    swoole

---
+++ Swoole编程须知/sleep.php
@@ -0,0 +1,22 @@
+<?php
+
+// 网络客户端/睡眠函数协程化
+Swoole\Runtime::enableCoroutine(true);
+
+$server = new Swoole\Server("0.0.0.0", 7749);
+
+// 单工作进程
+$server->set([
+    'worker_num' => 1,
+]);
+
+$server->on("Receive", function ($server, $fd, $reactorId, $data) {
+    // 验证有没有接收到客户端请求
+    $server->send($fd, "aaaa\n");
+
+    // 睡眠
+    sleep(5);
+    $server->send($fd, "Swoole: {$data}\n");
+});
+
+$server->start();
---
+++ 运行环境/README.md
@@ -0,0 +1,20 @@
+
+### 安装方式
+
+1.Linux + PHP +  源码编译 Swoole / PECL 安装 Swoole
+
+2.Linux + Docker + 镜像
+
+两种方式都可以，不影响脚本的运行，编译安装更直观，Docker 更快速。
+
+> Linux + Docker
+
+1.安装 Docker  
+（https://github.com/phvia/dkc#安装-docker）
+
+2.拉取镜像  
+（docker pull phvia/web-swoole-4.2.13:7.1.19-fpm）
+
+3.运行进入容器  
+（docker run -it -p 7749:7749 -v /home/ubuntu:/usr/share/nginx/html ImageID bash）
+
